c797ff0c411307b6d54b37875314950f35a20bb9
Ilya Misyura <54207353+MisyuraIlya@users.noreply.github.com>
2024-06-01 21:24:19 +0300

    split the common folder

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import jwt from 'jsonwebtoken'
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@spetsartickets/common';
 
 const router = express.Router();
 
