08cb8e78632577c14ef0b63e889789f1618e6827
ilya <misyurailya97@gmail.com>
2024-10-17 10:54:50 +0300

    test all2

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -62,7 +62,7 @@ const start = async () => {
     }
 
     app.listen(3000, () => {
-        console.log('Listening on port 3000!!!!');
+        console.log('Listening on port 3000!!!!!');
     });
 };
 
